f7d4886f90fa1a2eb6d6ae7f1b4a198b720c9040
Ifeoma <osegboifeoma3@gmail.com>
2026-02-06 15:52:03 +0100

    Add script.js file with initial film data and console log

---
+++ Data-flow-examples/script.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+const films = [
+  {
+    title: "Killing of Flower Moon",
+    director: "Martin Scorsese",
+    times: ["15:35"],
+    certificate: "15",
+    duration: 112,
+  },
+  {
+    title: "Typist Artist Pirate King",
+    directory: "Carol Morley",
+    times: ["15:00", "20:00"],
+    certificate: "12A",
+    duration: 108,
+  },
+];
+//render the films in the UI
