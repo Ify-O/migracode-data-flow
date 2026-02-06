11450711c64c1e95b29951d3056053283a0dbea9
Ifeoma <osegboifeoma3@gmail.com>
2026-02-06 17:03:38 +0100

    Implement film rendering logic and create UI elements for film title and director

--- Data-flow-examples/script.js
+++ Data-flow-examples/script.js
@@ -15,4 +15,17 @@ const films = [
     duration: 108,
   },
 ];
-//render the films in the UI
+console.log(films[0].title, "<----- films.title");
+//render the filmCard in the UI
+const filmCard = document.createElement("section"); //create an element
+//the "section" is the tag name of the element we want to create and it is a string. We can create any element we want by passing the tag name as an argument to the createElement method. is is also a div. 
+//filmCard.textContent = films[0].title; //append title to the element
+const title = document.createElement("h1") //creating a title element with h1
+const director = document.createElement("p"); //creating a director element with p
+director.textContent = films[0].director; //appending the director element to the filmCard element
+title.textContent = films[0].title; //appending the title element to the filmCard element
+document.body.appendChild(title);
+document.body.appendChild(director);
+
+document.body.appendChild(filmCard); //append the element to the body
+
